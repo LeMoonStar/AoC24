49c441a277a52ab4faa12e00f01f64bb4025ed58
LeMoonStar <webmaster@unitcore.de>
2024-12-01 06:37:02 +0100

    ✨ Day 1

--- src/days/d01.rs
+++ src/days/d01.rs
@@ -2,7 +2,7 @@ use super::{Answer, Day, DayImpl};
 
 const CURRENT_DAY: u8 = 1;
 
-type Data = Vec<u64>;
+type Data = Vec<[u64; 2]>;
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data) {
         Self::init(include_str!("test_inputs/test01.txt"))
@@ -17,16 +17,40 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
             Self {},
             input
                 .lines()
-                .map(|v| v.parse::<u64>().expect("error while parsing input."))
-                .collect(),
+                .map(|v| {
+                    v.split_whitespace()
+                        .map(|v| v.trim().parse::<u64>().expect("Error during parsing"))
+                        .collect::<Vec<u64>>()
+                        .try_into()
+                        .expect("Error during parsing")
+                })
+                .collect::<Vec<[u64; 2]>>(),
         )
     }
 
     fn one(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let mut left = data.iter().map(|v| v[0]).collect::<Vec<u64>>();
+        let mut right = data.iter().map(|v| v[1]).collect::<Vec<u64>>();
+
+        left.sort();
+        right.sort();
+
+        Answer::Number(
+            left.into_iter()
+                .zip(right.into_iter())
+                .map(|(a, b)| a.abs_diff(b))
+                .sum(),
+        )
     }
 
     fn two(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let mut left = data.iter().map(|v| v[0]).collect::<Vec<u64>>();
+        let mut right = data.iter().map(|v| v[1]).collect::<Vec<u64>>();
+
+        Answer::Number(
+            left.iter()
+                .map(|v| v * right.iter().filter(|n| *n == v).count() as u64)
+                .sum(),
+        )
     }
 }
